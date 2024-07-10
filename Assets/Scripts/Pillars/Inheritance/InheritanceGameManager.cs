691534994834d8fc21dd7f0e69360c3808f1736e
Mateo Alvear <70535124+Nselb@users.noreply.github.com>
2024-07-09 22:06:18 -0500

    Herencia ya

--- Assets/Scripts/Pillars/Inheritance/InheritanceGameManager.cs
+++ Assets/Scripts/Pillars/Inheritance/InheritanceGameManager.cs
@@ -1,18 +1,63 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class InheritanceGameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject winCanvas;
+    [SerializeField] private GameObject loseCanvas;
+    [SerializeField] private GameObject life1;
+    [SerializeField] private GameObject life2;
+    [SerializeField] private GameObject life3;
+    [SerializeField] private int maxPoints;
+    private int currentPoints;
+    private static InheritanceGameManager _instance;
+    public static InheritanceGameManager Instance
     {
-        
+        get
+        {
+            return _instance;
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+        if (_instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void Correct()
+    {
+        currentPoints += 1;
+        if (currentPoints == maxPoints)
+        {
+            winCanvas.SetActive(true);
+        }
+    }
+    public void Incorrect()
+    {
+        if (!life3.activeInHierarchy)
+        {
+            loseCanvas.SetActive(true);
+            Destroy(this);
+        }
+        if (!life2.activeInHierarchy)
+        {
+            life3.SetActive(false);
+        }
+        if (!life1.activeInHierarchy)
+        {
+            life2.SetActive(false);
+        }
+        if (life1.activeInHierarchy)
+        {
+            life1.SetActive(false);
+        }
     }
 }
